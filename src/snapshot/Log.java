c24cf48943efef1bb48e4acaa9becee4f2c60eae
Shubham Shah <shahshubham1888@gmail.com>
2026-03-08 23:28:46 +0530

    Enhance Log class to display corresponding files for each hash in the log output

--- src/snapshot/Log.java
+++ src/snapshot/Log.java
@@ -2,6 +2,7 @@ package snapshot;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
 import java.util.List;
 
 public class Log {
@@ -10,10 +11,20 @@ public class Log {
 
         Utils utils = new Utils();
         List<String> filePaths = utils.filePathsInDirectory(".snapshot/objects/refs/");
+
         for (String file : filePaths) {
             String content = Files.readString(Path.of(file));
-            String correspondFile = utils.hashCorrespondingFile(content);
-            System.out.println(correspondFile);
+            System.out.println(
+                    "Date : " + Instant.ofEpochMilli(Long.parseLong(content.split("\n")[0].replace("Time : ", "")))
+                            .atZone(java.time.ZoneId.systemDefault()));
+            System.out.println(content.split("\n")[1]);
+            String hashes = List.of(content.split("\n")).get(2).replace("[", "").replace("]", "").replace(" ", "");
+            List<String> hashList = List.of(hashes.split(","));
+            for (String hash : hashList) {
+                String correspondFile = utils.hashCorrespondingFile(hash);
+                System.out.println(correspondFile);
+            }
+            System.out.println("--------------------------------------------------");
         }
     }
 }
